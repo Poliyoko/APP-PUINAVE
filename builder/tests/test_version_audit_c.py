a3f7618cfad2b5c34927a0fcb965a643ae10218b
POLIYOKO <MTMUDJGSA@GMAIL.COM>
2026-07-14 03:27:56 -0500

    fix(ci): actualizar pruebas a version 0.10.0

--- builder/tests/test_version_audit_c.py
+++ builder/tests/test_version_audit_c.py
@@ -1,6 +1,7 @@
-from sgoda.cli.main import main
+﻿from sgoda.cli.main import main
 
 
 def test_version(capsys) -> None:
     assert main(["version"]) == 0
-    assert "0.9.0" in capsys.readouterr().out
+    assert "0.10.0" in capsys.readouterr().out
+
